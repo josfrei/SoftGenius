c1878c5850fefbe0a01ec4c8423727b5ac6e71e5
josef <josef@LAPTOP-22O96BBN>
2024-04-16 22:05:15 +0200

    Inicio programa

---
+++ Documents/NetBeansProjects/SoftGenius/src/softgenius/SoftGenius.java
@@ -0,0 +1,16 @@
+package softgenius;
+
+/**
+ *
+ * @author iago adrián david federico fran patrick josef
+ */
+public class SoftGenius {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
