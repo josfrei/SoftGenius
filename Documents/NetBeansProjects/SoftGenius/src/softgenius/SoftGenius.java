b7ee238a5ffbd90264e048dd52373337036ab810
josef <josef@LAPTOP-22O96BBN>
2024-04-18 17:23:57 +0200

    La última

--- Documents/NetBeansProjects/SoftGenius/src/softgenius/SoftGenius.java
+++ Documents/NetBeansProjects/SoftGenius/src/softgenius/SoftGenius.java
@@ -10,7 +10,7 @@ public class SoftGenius {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Vamos a hacer una prueba más
+        // Yahora la última
     }
     
 }
