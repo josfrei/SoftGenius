f46986180d587359d907760e779d05c3a7e6da17
josef <josef@LAPTOP-22O96BBN>
2024-04-18 17:02:07 +0200

    aaaak

--- Documents/NetBeansProjects/SoftGenius/src/softgenius/SoftGenius.java
+++ Documents/NetBeansProjects/SoftGenius/src/softgenius/SoftGenius.java
@@ -8,7 +8,7 @@ public class SoftGenius {
 
     /**
      * @param args the command line arguments
-     */aaaa
+     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
