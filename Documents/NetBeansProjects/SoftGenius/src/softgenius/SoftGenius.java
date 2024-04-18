1f0cc2eb1315ea4b3b67687b89cfa48f4fc36d6e
josef <josef@LAPTOP-22O96BBN>
2024-04-18 17:02:59 +0200

    prueba

--- Documents/NetBeansProjects/SoftGenius/src/softgenius/SoftGenius.java
+++ Documents/NetBeansProjects/SoftGenius/src/softgenius/SoftGenius.java
@@ -10,7 +10,7 @@ public class SoftGenius {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here. Prueba
     }
     
 }
